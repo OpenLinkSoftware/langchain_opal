f2a99ffd00ef0853d13926f1e112149775d07761
Sergey Malinin <smalinin@openlinksw.com>
2024-10-21 17:06:51 +0300

    Update version to 0.1.0

--- langchain_opal/__init__.py
+++ langchain_opal/__init__.py
@@ -9,7 +9,7 @@ try:
     __version__ = metadata.version(__package__)
 except metadata.PackageNotFoundError:
     # Case where package metadata is not available.
-    __version__ = "0.0.2"
+    __version__ = "0.1.0"
 del metadata  # optional, avoids polluting the results of dir(__package__)
 
 __all__ = [
